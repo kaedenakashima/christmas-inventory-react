b40aecd8ad964689a78d57a3be614d014bb9da9f
Kaede Nakashima <kaede.nakashima@yahoo.com>
2019-12-31 06:05:31 +0900

    Update inventory.js

--- src/data/inventory.js
+++ src/data/inventory.js
@@ -1,176 +1,176 @@
-export default[
-	{
-		name: "Christmas Ball Ornaments",
-		concept: "original",
-		size: "3.15",
-		material: "plastic",
-		type: "Tree decolations",
-		details: [
-			{
-				color: "red",
-				setof: 12,
-				price: 12,
-				stock: 200,
-			},
-			{
-				color: "green",
-				setof: 12,
-				price: 12,
-				stock: 200,
-			},
-			{
-				color: "blue",
-				setof: 12,
-				price: 12,
-				stock: 150,
-			},
-			{
-				color: "pink",
-				setof: 12,
-				price: 24,
-				stock: 150,
-			},
-			{
-				color: "yellow",
-				setof: 12,
-				price: 12,
-				stock: 150,
-			}
-		]
-	},
-	{
-		name: "Christmas Ball Ornaments",
-		concept: "gorgeous",
-		size: "3.15",
-		material: "plastic",
-		type: "Tree decolations",
-		details: [
-			{
-				color: "gold",
-				setof: 12,
-				price: 15,
-				stock: 100,
-			},
-			{
-				color: "silver",
-				setof: 12,
-				price: 15,
-				stock: 100,
-			},
-			{
-				color: "champagne",
-				setof: 12,
-				price: 15,
-				stock: 100,
-			}
-		]
-	},
-	{
-		name: "Christmas Ball Ornaments",
-		concept: "pastel",
-		size: "3.15",
-		material: "plastic",
-		type: "Tree decolations",
-		details: [
-			{
-				color: "peppermint",
-				setof: 6,
-				price: 10,
-				stock: 20,
-			},
-			{
-				color: "lavender purple",
-				setof: 6,
-				price: 10,
-				stock: 10,
-			},
-			{
-				color: "rose pink",
-				setof: 6,
-				price: 10,
-				stock: 13,
-			},
-			{
-				color: "teal",
-				setof: 6,
-				price: 10,
-				stock: 13,
-			}
-		]
-	},
-	{
-		name: "Christmas swarovski Ornament",
-		concept: "premiere",
-		size: "4.5",
-		material: "swarovski",
-		type: "Tree decolations",
-		details: [
-			{
-				color: "clear",
-				setof: 1,
-				price: 74,
-				stock: 20,
-			}
-		]
-	},
-	{
-		name: "Christmas Santa Ornament",
-		concept: "chrismassy",
-		size: "",
-		material: "",
-		type: "Tree decolations",
-		details: [
-			{
-				color: "red",
-				setof: 1,
-				price: 7,
-				stock: 300,
-			}
-		]
-	},
-	{
-		name: "Christmas Santa Ornament",
-		concept: "chrismassy",
-		size: "",
-		material: "",
-		type: "Tree decolations",
-		details: [
-			{
-				color: "red",
-				setof: 1,
-				price: 7,
-				stock: 300,
-			}
-		]
-	},
-	{
-		name: "Large Christmas Tree",
-		concept: "traditional",
-		size: "7.5foot",
-		material: "pine",
-		type: "Christmas Tree",
-		details: [
-			{
-				color: "green",
-				setof: 1,
-				price: 120,
-				stock: 20,
-			}
-		]
-	},
-	{
-		name: "Large Christmas Nutcracker",
-		concept: "traditional",
-		size: "32.7",
-		material: "wood",
-		type: "Christmas Decorations for Fireplace",
-		details: [
-			{
-				color: "red",
-				setof: 1,
-				price: 68,
-				stock: 20,
-			}
-		]
-	},
-]
+export default [
+  {
+    name: "Large Christmas Tree",
+    concept: "traditional",
+    size: "7.5foot",
+    material: "pine",
+    type: "Christmas Tree",
+    details: [
+      {
+        color: "green",
+        setof: 1,
+        price: 120,
+        stock: 20
+      }
+    ]
+  },
+  {
+    name: "Large Christmas Nutcracker",
+    concept: "traditional",
+    size: "32.7",
+    material: "wood",
+    type: "Christmas Decorations for Fireplace",
+    details: [
+      {
+        color: "red",
+        setof: 1,
+        price: 68,
+        stock: 20
+      }
+    ]
+  },
+  {
+    name: "Christmas Santa Ornament",
+    concept: "chrismassy",
+    size: "",
+    material: "",
+    type: "Tree decolations",
+    details: [
+      {
+        color: "red",
+        setof: 1,
+        price: 7,
+        stock: 300
+      }
+    ]
+  },
+  {
+    name: "Christmas Reindeer Ornament",
+    concept: "chrismassy",
+    size: "",
+    material: "",
+    type: "Tree decolations",
+    details: [
+      {
+        color: "red",
+        setof: 1,
+        price: 7,
+        stock: 300
+      }
+    ]
+  },
+  {
+    name: "Christmas swarovski Ornament",
+    concept: "premiere",
+    size: "4.5",
+    material: "swarovski",
+    type: "Tree decolations",
+    details: [
+      {
+        color: "clear",
+        setof: 1,
+        price: 74,
+        stock: 20
+      }
+    ]
+  },
+  {
+    name: "Christmas Ball Ornaments",
+    concept: "original",
+    size: "3.15",
+    material: "plastic",
+    type: "Tree decolations",
+    details: [
+      {
+        color: "red",
+        setof: 12,
+        price: 12,
+        stock: 200
+      },
+      {
+        color: "green",
+        setof: 12,
+        price: 12,
+        stock: 200
+      },
+      {
+        color: "blue",
+        setof: 12,
+        price: 12,
+        stock: 150
+      },
+      {
+        color: "pink",
+        setof: 12,
+        price: 24,
+        stock: 150
+      },
+      {
+        color: "yellow",
+        setof: 12,
+        price: 12,
+        stock: 150
+      }
+    ]
+  },
+  {
+    name: "Christmas Ball Ornaments",
+    concept: "gorgeous",
+    size: "3.15",
+    material: "plastic",
+    type: "Tree decolations",
+    details: [
+      {
+        color: "gold",
+        setof: 12,
+        price: 15,
+        stock: 100
+      },
+      {
+        color: "silver",
+        setof: 12,
+        price: 15,
+        stock: 100
+      },
+      {
+        color: "champagne",
+        setof: 12,
+        price: 15,
+        stock: 100
+      }
+    ]
+  },
+  {
+    name: "Christmas Ball Ornaments",
+    concept: "pastel",
+    size: "3.15",
+    material: "plastic",
+    type: "Tree decolations",
+    details: [
+      {
+        color: "peppermint",
+        setof: 6,
+        price: 10,
+        stock: 20
+      },
+      {
+        color: "lavender purple",
+        setof: 6,
+        price: 10,
+        stock: 10
+      },
+      {
+        color: "rose pink",
+        setof: 6,
+        price: 10,
+        stock: 13
+      },
+      {
+        color: "teal",
+        setof: 6,
+        price: 10,
+        stock: 13
+      }
+    ]
+  }
+];
